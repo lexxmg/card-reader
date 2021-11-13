ec7f7ddf91b6d84cf222a141c2fde0d1af666177
mac-book <lexx.mg@gmail.com>
2021-11-13 13:33:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+const { createWorker } = Tesseract;
+(async () => {
+  const worker = createWorker();
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  await worker.setParameters({
+    tessedit_char_whitelist: '0123456789',
+  });
+  const { data: { text } } = await worker.recognize('../../img/Изображение PNG.png');
+  console.log(text.substr(0, 10));
+  await worker.terminate();
+})();
